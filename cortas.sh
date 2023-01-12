b40322827b022a56f4fee2fc57e6b343fa2cd0fe
davidrf <davidrf@zdn.es>
2023-01-12 14:03:05 +0100

    Cache control

--- cortas.sh
+++ cortas.sh
@@ -4,11 +4,10 @@ FILES=`dirname $0`/archivos
 ARCHIVO=`echo ${SCRIPT_NAME} | tr -d '/-'`
 URLFILE="${FILES}/${ARCHIVO}"
 if [ -f ${URLFILE} ] ; then
-  printf "Status: 301 Moved Permanently\r\n";
-  echo -e "Location: "`cat -- ${URLFILE}`"\r\n\r\n"
+  printf "Status: 301 Moved Permanently\r\n"
+  printf "cache-control: private, max-age=2592000\r\n"
+  printf "Location: "`cat -- ${URLFILE} | tr -d '\r\n'`"\r\n\r\n"
 else
   printf "Status: 404 Not found\r\n\r\n";
   echo No encontrado
 fi
-
-# printf "content-type: text/plain\r\n\r\n"
